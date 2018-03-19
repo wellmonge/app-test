18f26a2ef7970ee01ec0d31d8d5d2a741a3fcce6
exame <exame@tjmt.jus.br>
2018-03-19 16:13:54 -0400

    ...

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// using ES6 modules
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+import clientScreen from "./components/clientScreen";
+// import homeScreen from "./components/homeScreen";
+
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Teste TJ</h1>
+          <nav>
+            {/* <Link to="/">Home</Link> */}
+            <Link to="/client">Client</Link>
+          </nav>
         </header>
-        
-    
+      
+
+        <div>
+          <Route path="/client" component={clientScreen}/>
+        </div>
+        <div>
+
+          {/* <Route path="/" component={homeScreen}/> */}
+        </div>
       </div>
-    );
+      </BrowserRouter>
+    )
   }
 }
 
--- src/components/clientScreen.js
+++ src/components/clientScreen.js
@@ -1,11 +1,28 @@
 import React, { Component  } from "react";
 
 
-class Home extends Component() {
+
+class Client extends Component {
+
+    renderLista = () =>{
+        const lista = fetch("http://localhost:50317/api/client/AllClients").then(res => {
+            console.log(res);
+        }).catch(Error =>{
+            
+        });
+
+    }
+    
+
+    
     render() {
-        return(<div/>)
+        return(<div>            
+            {this.renderLista()}
+        </div>)
 
     }
 
     
 }
+
+export default Client
